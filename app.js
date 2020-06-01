eff9051f1e0cdfde2ba443a8adc37db04d4de59b
Sherry McCarty <sherrydmccarty@gmail.com>
2020-06-01 17:31:14 -0400

    Solution

--- app.js
+++ app.js
@@ -83,6 +83,40 @@ app.get('/cipher', (req, res) => {
   res.send(shiftedText);
 })
 
+app.get('/lotto', (req, res) => {
+  const { numbers } = req.query;
+
+  const guesses = numbers.map(n => parseInt(n).filter(n => !Number.isNaN(n) && n <= 20));
+
+  const stockNumbers = Array(20).fill(1).map((_, i) => i + 1);
+  const winningNumbers = [];
+  for(let i = 0; i < 6; i++) {
+    const ran = Math.floor(Math.random() * stockNumbers.length);
+    winningNumbers.push(stockNumbers[ran]);
+    stockNumbers.splice(ran, 1);
+  }
+
+  let diff = winningNumbers.filter(n => !guesses.includes(n));
+
+  let responseText;
+
+  switch(diff.length) {
+    case 0: 
+      responseText = 'Wow! Unbelieveable! You could have won the mega millions!';
+      break;
+    case 1:
+      responseText = 'Congratulations! You won $100!';
+      break;
+    case 2:
+      responseText = 'Congratulations, you win a free ticket!';
+      break;
+    default:
+      responseText = 'Sorry, you lose';
+  }
+
+  res.send(responseText);
+})
+
 app.listen(8080, () => {
   console.log('Express server is listening on port 8080!');
 })
